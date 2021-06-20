943f1b47098cf1d70853ae0956f74d20b7717ee7
Felipe <felipeffggomes@hotmail.com>
2021-06-20 14:06:07 -0300

    Ajustes Realizados

--- src/main/java/com/sistema/gestao/repository/AlunoTurmaRepostory.java
+++
@@ -1,8 +0,0 @@
-package com.sistema.gestao.repository;
-
-import com.sistema.gestao.model.AlunoTurma;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoTurmaRepostory extends JpaRepository<AlunoTurma,String> {
-
-}
